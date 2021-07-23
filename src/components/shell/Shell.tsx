7bda70754c658379707f22b4363ac63249a1d404
Shashank Kaul <kaulshashank96@gmail.com>
2021-07-24 00:30:02 +0530

    build: Migrated from CRA to vite.js

--- src/components/shell/Shell.tsx
+++ src/components/shell/Shell.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Editor, { useMonaco } from "@monaco-editor/react";
 import { registerCompletions } from "./completions";
 
