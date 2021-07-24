50e3a8d0f6fd2658ca8e672d6677b6a2f499e4ac
Rishi Kabra <rishikabra132@gmail.com>
2021-07-24 16:58:15 +0530

    updated window.ts

--- electron/window.ts
+++ electron/window.ts
@@ -1,13 +1,16 @@
 import { BrowserWindowConstructorOptions, BrowserWindow } from "electron";
 
-interface CreateWindowOptions { }
-function createWindow(windowOptions?: BrowserWindowConstructorOptions, options?: CreateWindowOptions) {
-    const window = new BrowserWindow(windowOptions);
-    window.removeMenu();
-    window.webContents.openDevTools();
-    return window;
+interface CreateWindowOptions {}
+function createWindow(
+	windowOptions?: BrowserWindowConstructorOptions,
+	options?: CreateWindowOptions
+) {
+	const window = new BrowserWindow(windowOptions);
+	window.removeMenu();
+	window.webContents.openDevTools();
+	return window;
 }
 
 export default {
-    createWindow
-}
+	createWindow,
+};
