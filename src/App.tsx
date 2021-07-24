bc31a7276a5512abd1a014b916bd2ba7eb922904
Rishi Kabra <rishikabra132@gmail.com>
2021-07-24 16:59:39 +0530

    updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,12 @@ import React from "react";
 import { Resizable } from "re-resizable";
 import "./App.less";
 
-import Shell from "./components/shell/Shell";
-import Explorer from "./components/explorer/Explorer";
+import { Layout } from "./components/layout/layout";
 
 function App() {
 	return (
 		<div className="App">
-			<Resizable
-				defaultSize={{
-					width: "25%",
-					height: "100%",
-				}}
-			>
-				<Explorer />
-			</Resizable>
-			<Resizable
-				defaultSize={{
-					width: "75%",
-					height: "100%",
-				}}
-			>
-				<Shell
-					onExecute={(code) => {
-						console.log("Code to be executed:");
-						console.log(code);
-						window.ark.api.ping();
-					}}
-					collections={["test_collection_list_1", "test_collection_list_2"]}
-				/>
-			</Resizable>
+			<Layout />
 		</div>
 	);
 }
