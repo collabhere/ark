3e1f3505759e4310e184e0aafd961feb9a68ec5f
Abhishek Kumar <abhishek.kumar251095@gmail.com>
2021-07-25 11:46:58 +0530

    feat(connections): connection using mongo uri

--- electron/stores/connection.ts
+++ electron/stores/connection.ts
@@ -1,26 +1,33 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
+import { nanoid } from "nanoid";
 
 const store = new Map<string, MongoClient>();
 
 export const connectionStore = () => {
+	const saveConnection = (connection: MongoClient) => {
+		if (connection) {
+			const id = nanoid();
+			store.set(id, connection);
 
-    const setConnection = (name: string, connection: MongoClient) => {
-        if (name && connection) {
-            return store.set(name, connection);            
-        }
-    }
+			return id;
+		} else {
+			throw new Error("Unable to save an empty connection!");
+		}
+	};
 
-    const getConnection = (name: string) => {
-        if (store.has(name)) {
-            return store.get(name);
-        }
-    }
+	const getConnection = (id: string): MongoClient => {
+		if (store.has(id)) {
+			return store.get(id);
+		} else {
+			throw new Error(`No connection exists with id: ${id}!`);
+		}
+	};
 
-    const deleteConnection = (name: string) => {
-        if (store.has(name)) {
-            return store.delete(name);
-        }
-    }
+	const deleteConnection = (id: string) => {
+		if (store.has(id)) {
+			return store.delete(id);
+		}
+	};
 
-    return { getConnection, setConnection, deleteConnection }
-}
+	return { getConnection, saveConnection, deleteConnection };
+};
