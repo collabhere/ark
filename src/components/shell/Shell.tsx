68f26af8bb99df30bd415092931334b70c0cae2c
Abhishek Kumar <abhishek.kumar251095@gmail.com>
2021-10-09 20:15:46 +0530

    refactor(exports): moved exports within result-viewer

--- src/components/shell/Shell.tsx
+++ src/components/shell/Shell.tsx
@@ -5,9 +5,6 @@ import Monaco from "@monaco-editor/react";
 import { KeyMod, KeyCode, editor } from "monaco-editor";
 import { mountMonaco } from "./monaco";
 import { Button } from "../../common/components/Button";
-import { Dialog } from "../../common/components/Dialog";
-import { Input, Radio, Switch } from "antd";
-import TextArea from "antd/lib/input/TextArea";
 
 export enum MONACO_COMMANDS {
 	CLONE_SHELL,
@@ -16,90 +13,23 @@ export enum MONACO_COMMANDS {
 
 export interface ShellProps {
 	allCollections: string[];
-	initialCode: string;
-	onMonacoCommand?: (command: MONACO_COMMANDS, params?: any) => void;
-	onExport?: (params?: any) => void;
+	code: string;
+	onCodeChange: (code: string) => void;
+	onMonacoCommand?: (command: MONACO_COMMANDS) => void;
 }
 export const Shell: FC<ShellProps> = (props) => {
-	const { allCollections, onMonacoCommand, initialCode, onExport } = props;
-
-	const [code, setCode] = useState(initialCode);
+	const { allCollections, onMonacoCommand, code, onCodeChange } = props;
 
 	const [monacoEditor, setMonacoEditor] =
 		useState<editor.IStandaloneCodeEditor>();
 
-	const [exportDialog, toggleExportDialog] = useState<boolean>(false);
-
-	const [exportOptions, setExportOptions] = useState<
-		Ark.ExportNdjsonOptions | Ark.ExportCsvOptions
-	>({
-		type: "NDJSON",
-		fileName: "",
-	});
-
 	const exec = useCallback(() => {
-		const _code = code.replace(/(\/\/.*)|(\n)/g, "");
-		onMonacoCommand &&
-			onMonacoCommand(MONACO_COMMANDS.EXEC_CODE, { code: _code });
-	}, [code, onMonacoCommand]);
-
-	const exportData = useCallback(() => {
-		const _code = code.replace(/(\/\/.*)|(\n)/g, "");
-		onExport && onExport({ code: _code, options: exportOptions });
-		toggleExportDialog(false);
-		setExportOptions({
-			type: "NDJSON",
-			fileName: "",
-		});
-	}, [code, exportOptions, onExport]);
-
-	const changeExportOptions = useCallback(
-		(option: "fields" | "destructure" | "type" | "fileName", e?: any) => {
-			if (option === "type") {
-				if (e.target.value === "CSV") {
-					setExportOptions((options) => ({
-						...options,
-						type: "CSV",
-						destructureData: false,
-						fields: [],
-					}));
-				} else if (e.target.value === "NDJSON") {
-					setExportOptions((options) => ({
-						fileName: options.fileName,
-						type: "NDJSON",
-					}));
-				}
-			} else if (option === "destructure") {
-				setExportOptions((options) => ({
-					...options,
-					destructureData:
-						options.type === "CSV" ? !options.destructureData : false,
-				}));
-			} else if (option === "fileName") {
-				setExportOptions((options) => ({
-					...options,
-					fileName: e.target.value,
-				}));
-			} else {
-				setExportOptions((options) => ({
-					...options,
-					fields:
-						options.type === "CSV"
-							? e.target.value.split(",").map((field) => field.trim())
-							: undefined,
-				}));
-			}
-		},
-		[]
-	);
+		onMonacoCommand && onMonacoCommand(MONACO_COMMANDS.EXEC_CODE);
+	}, [onMonacoCommand]);
 
 	const cloneCurrentTab = useCallback(() => {
 		onMonacoCommand && onMonacoCommand(MONACO_COMMANDS.CLONE_SHELL);
 	}, [onMonacoCommand]);
-
-	useEffect(() => {
-		setCode(initialCode);
-	}, [initialCode]);
 
 	useEffect(() => {
 		if (monacoEditor) {
@@ -113,70 +43,6 @@ export const Shell: FC<ShellProps> = (props) => {
 
 	return (
 		<div className={"Shell"}>
-			<Button text="Export" onClick={() => toggleExportDialog(true)} />
-			{exportDialog && (
-				<Dialog
-					size={"small"}
-					title={"Export Query Result"}
-					onConfirm={exportData}
-					onCancel={() => toggleExportDialog(false)}
-				>
-					<div className={"export-options"}>
-						<div className={"export-type"}>
-							<div>
-								<span>Export as: </span>
-							</div>
-							<div>
-								<Radio.Group
-									options={["CSV", "NDJSON"]}
-									value={exportOptions.type}
-									buttonStyle={"solid"}
-									onChange={(e) => {
-										changeExportOptions("type", e);
-									}}
-								/>
-							</div>
-						</div>
-						<div className={"export-type"}>
-							<div>
-								<span>File name: </span>
-							</div>
-							<div>
-								<Input
-									value={exportOptions.fileName}
-									onChange={(e) => changeExportOptions("fileName", e)}
-								/>
-							</div>
-						</div>
-						{exportOptions.type === "CSV" && (
-							<div>
-								<div className={"export-suboptions"}>
-									<div>
-										<span>Destructure data: </span>
-									</div>
-									<div>
-										<Switch
-											checked={!!exportOptions.destructureData}
-											onChange={() => changeExportOptions("destructure")}
-										/>
-									</div>
-								</div>
-								<div>
-									<div>
-										<span>Fields: </span>
-									</div>
-									<div>
-										<TextArea
-											value={exportOptions.fields?.join(",")}
-											onChange={(e) => changeExportOptions("fields", e)}
-										/>
-									</div>
-								</div>
-							</div>
-						)}
-					</div>
-				</Dialog>
-			)}
 			<Monaco
 				options={{
 					minimap: {
@@ -204,7 +70,7 @@ export const Shell: FC<ShellProps> = (props) => {
 					setMonacoEditor(editor);
 				}}
 				onChange={(value, ev) => {
-					value && setCode(value);
+					value && onCodeChange(value);
 				}}
 				height="100%"
 				defaultValue={code}
