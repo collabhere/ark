393014d2ba192411ef92191708a1e6334e796c93
Shashank Kaul <kaulshashank96@gmail.com>
2021-10-23 12:53:40 +0530

    feat(hooks): useRefresh added

---
+++ src/hooks/useRefresh.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface UseRefresh {
+    (): [number, () => void];
+}
+
+export const useRefresh: UseRefresh = () => {
+    const [refToken, setToken] = React.useState(0);
+    return [refToken, () => setToken(refToken + 1)];
+}
