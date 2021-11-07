468ff38b37bb75e851bb0a1428d758c1807a8a1b
Shashank Kaul <kaulshashank96@gmail.com>
2021-11-07 19:35:30 +0530

    feat(monaco): Sorted out required typing changes for the editor.

--- src/components/shell/mongo-shell-completion.ts
+++ src/components/shell/mongo-shell-completion.ts
@@ -1,22 +1,6 @@
 
 import MONGO_SHELL_GLOBAL_DEFINITION from "../../mongoshell.d.ts?raw";
 
-import MONGO_SHELL_CORE_ALL_FLE_TYPES from "@mongosh/service-provider-core/lib/all-fle-types.d.ts?raw";
-import MONGO_SHELL_CORE_ALL_TRANSPORT_TYPES from "@mongosh/service-provider-core/lib/all-transport-types.d.ts?raw";
-import MONGO_SHELL_CORE_ADMIN from "@mongosh/service-provider-core/lib/admin.d.ts?raw";
-import MONGO_SHELL_CORE_CLI_OPTIONS from "@mongosh/service-provider-core/lib/cli-options.d.ts?raw";
-import MONGO_SHELL_CORE_CLOSABLE from "@mongosh/service-provider-core/lib/closable.d.ts?raw";
-import MONGO_SHELL_CORE_CONNECT_INFO from "@mongosh/service-provider-core/lib/connect-info.d.ts?raw";
-import MONGO_SHELL_CORE_FAST_FAILURE_CONNECT from "@mongosh/service-provider-core/lib/fast-failure-connect.d.ts?raw";
-import MONGO_SHELL_CORE_INDEX from "@mongosh/service-provider-core/lib/index.d.ts?raw";
-import MONGO_SHELL_CORE_PLATFORM from "@mongosh/service-provider-core/lib/platform.d.ts?raw";
-import MONGO_SHELL_CORE_PRINTABLE_BSON from "@mongosh/service-provider-core/lib/printable-bson.d.ts?raw";
-import MONGO_SHELL_CORE_READABLE from "@mongosh/service-provider-core/lib/readable.d.ts?raw";
-import MONGO_SHELL_CORE_SERVICE_PROVIDER from "@mongosh/service-provider-core/lib/service-provider.d.ts?raw";
-import MONGO_SHELL_CORE_SHELL_AUTH_OPTIONS from "@mongosh/service-provider-core/lib/shell-auth-options.d.ts?raw";
-import MONGO_SHELL_CORE_TEXTENCODER_POLYFILL from "@mongosh/service-provider-core/lib/textencoder-polyfill.d.ts?raw";
-import MONGO_SHELL_CORE_URI_GENERATOR from "@mongosh/service-provider-core/lib/uri-generator.d.ts?raw";
-import MONGO_SHELL_CORE_WRITABLE from "@mongosh/service-provider-core/lib/writable.d.ts?raw";
 import MONGO_SHELL_API_ABSTRACT_CURSOR from "@mongosh/shell-api/lib/abstract-cursor.d.ts?raw";
 import MONGO_SHELL_API_AGGREGATION_CURSOR from "@mongosh/shell-api/lib/aggregation-cursor.d.ts?raw";
 import MONGO_SHELL_API_BULK from "@mongosh/shell-api/lib/bulk.d.ts?raw";
@@ -26,7 +10,6 @@ import MONGO_SHELL_API_CURSOR from "@mongosh/shell-api/lib/cursor.d.ts?raw";
 import MONGO_SHELL_API_DATABASE from "@mongosh/shell-api/lib/database.d.ts?raw";
 import MONGO_SHELL_API_DBQUERY from "@mongosh/shell-api/lib/dbquery.d.ts?raw";
 import MONGO_SHELL_API_DECORATORS from "@mongosh/shell-api/lib/decorators.d.ts?raw";
-import MONGO_SHELL_API_DEPRECATION_WARNING from "@mongosh/shell-api/lib/deprecation-warning.d.ts?raw";
 import MONGO_SHELL_API_ENUMS from "@mongosh/shell-api/lib/enums.d.ts?raw";
 import MONGO_SHELL_API_ERROR_CODES from "@mongosh/shell-api/lib/error-codes.d.ts?raw";
 import MONGO_SHELL_API_EXPLAINABLE_CURSOR from "@mongosh/shell-api/lib/explainable-cursor.d.ts?raw";
@@ -46,26 +29,10 @@ import MONGO_SHELL_API_SESSION from "@mongosh/shell-api/lib/session.d.ts?raw";
 import MONGO_SHELL_API_SHARD from "@mongosh/shell-api/lib/shard.d.ts?raw";
 import MONGO_SHELL_API_SHELL_API from "@mongosh/shell-api/lib/shell-api.d.ts?raw";
 import MONGO_SHELL_API_SHELL_BSON from "@mongosh/shell-api/lib/shell-bson.d.ts?raw";
-import MONGO_SHELL_API_SHELL_INTERNAL_STATE from "@mongosh/shell-api/lib/shell-internal-state.d.ts?raw";
+import MONGO_SHELL_API_SHELL_INSTANCE_STATE from "@mongosh/shell-api/lib/shell-instance-state?raw";
 import { Monaco } from "@monaco-editor/react";
 
 const MONGO_SHELL_TYPE_DEFINITIONS = [
-    { name: "all-fle-types.d.ts", code: MONGO_SHELL_CORE_ALL_FLE_TYPES },
-    { name: "all-transport-types.d.ts", code: MONGO_SHELL_CORE_ALL_TRANSPORT_TYPES },
-    { name: "admin.d.ts", code: MONGO_SHELL_CORE_ADMIN },
-    { name: "cli-options.d.ts", code: MONGO_SHELL_CORE_CLI_OPTIONS },
-    { name: "closable.d.ts", code: MONGO_SHELL_CORE_CLOSABLE },
-    { name: "connect-info.d.ts", code: MONGO_SHELL_CORE_CONNECT_INFO },
-    { name: "fast-failure-connect.d.ts", code: MONGO_SHELL_CORE_FAST_FAILURE_CONNECT },
-    { name: "index.d.ts", code: MONGO_SHELL_CORE_INDEX },
-    { name: "platform.d.ts", code: MONGO_SHELL_CORE_PLATFORM },
-    { name: "printable-bson.d.ts", code: MONGO_SHELL_CORE_PRINTABLE_BSON },
-    { name: "readable.d.ts", code: MONGO_SHELL_CORE_READABLE },
-    { name: "service-provider.d.ts", code: MONGO_SHELL_CORE_SERVICE_PROVIDER },
-    { name: "shell-auth-options.d.ts", code: MONGO_SHELL_CORE_SHELL_AUTH_OPTIONS },
-    { name: "textencoder-polyfill.d.ts", code: MONGO_SHELL_CORE_TEXTENCODER_POLYFILL },
-    { name: "uri-generator.d.ts", code: MONGO_SHELL_CORE_URI_GENERATOR },
-    { name: "writable.d.ts", code: MONGO_SHELL_CORE_WRITABLE },
     { name: "abstract-cursor.d.ts", code: MONGO_SHELL_API_ABSTRACT_CURSOR, },
     { name: "aggregation-cursor.d.ts", code: MONGO_SHELL_API_AGGREGATION_CURSOR, },
     { name: "bulk.d.ts", code: MONGO_SHELL_API_BULK, },
@@ -75,7 +42,6 @@ const MONGO_SHELL_TYPE_DEFINITIONS = [
     { name: "database.d.ts", code: MONGO_SHELL_API_DATABASE, },
     { name: "dbquery.d.ts", code: MONGO_SHELL_API_DBQUERY, },
     { name: "decorators.d.ts", code: MONGO_SHELL_API_DECORATORS, },
-    { name: "deprecation-warning.d.ts", code: MONGO_SHELL_API_DEPRECATION_WARNING, },
     { name: "enums.d.ts", code: MONGO_SHELL_API_ENUMS, },
     { name: "error-codes.d.ts", code: MONGO_SHELL_API_ERROR_CODES, },
     { name: "explainable-cursor.d.ts", code: MONGO_SHELL_API_EXPLAINABLE_CURSOR, },
@@ -95,19 +61,25 @@ const MONGO_SHELL_TYPE_DEFINITIONS = [
     { name: "shard.d.ts", code: MONGO_SHELL_API_SHARD, },
     { name: "shell-api.d.ts", code: MONGO_SHELL_API_SHELL_API, },
     { name: "shell-bson.d.ts", code: MONGO_SHELL_API_SHELL_BSON, },
-    { name: "shell-internal-state.d.ts?raw", code: MONGO_SHELL_API_SHELL_INTERNAL_STATE },
+    { name: "shell-internal-state.d.ts?raw", code: MONGO_SHELL_API_SHELL_INSTANCE_STATE },
 ];
 
-const TS_PROMISE_GENERIC_RGX = /(Promise<(?=.*))|(>(?=;))/gim;
+// Should match:
+// Promise<Document | null>;
+// Promise<Document ? >;
+// Promise<Document>;
+// Promise<Document[]>;
+// Promise<Array<Document[]>>;
+// forEach(f: (doc: Document) => void | boolean | Promise<void   > | Promise<boolean>): Promise<void>;
+// Should not match:
+// AbstractCursor<ServiceProviderCursor>;
+// const TS_PROMISE_GENERIC_RGX = /(Promise<(?=.*))|((?<=[\s\w])>(?=;|\s|\)))/gim;
+const TS_PROMISE_GENERIC_RGX = /(Promise<(?=[\w[\]]+))|((?<=(Promise<[\s\w[\]|?<]+))>)/gim;
 
 function modifyMongoShellLibCode(code: string): string {
     return code
         // We do not need any promises in the definition files
-        .replace(TS_PROMISE_GENERIC_RGX, '')
-        // Since cursor methods are written in a sync
-        // fashion, i.e. db.collection.find().map(...),
-        // we want the types to allow for chaining array methods with cursor methods.
-        .replace(/(?<=\s)Cursor(?=;)/gim, '(Cursor & Array<Document>)');
+        .replace(TS_PROMISE_GENERIC_RGX, '');
 }
 
 export function addMongoShellCompletions(monaco: Monaco): void {
--- src/mongoshell.d.ts
+++ src/mongoshell.d.ts
@@ -12,4 +12,8 @@ declare global {
     export const Long: typeof bson.Long;
     export const Decimal128: typeof bson.Decimal128;
     export const Timestamp: typeof bson.Timestamp;
+    // ignore the error that this causes in intellisense. Monaco is configured to
+    // not have default javascript types and instead we are adding types for 
+    // globals that are provided by mongosh.
+    export const Date: typeof global.Date;
 }
