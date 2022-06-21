79a690c94afff4cefa95f8a5fef82abae9eb05b5
Shashank Kaul <kaulshashank96@gmail.com>
2022-06-21 21:05:21 +0530

    feat(utils): Extracted formatBSON into global utils

--- util/misc.ts
+++ util/misc.ts
@@ -1,3 +1,6 @@
+import { ObjectId } from "bson";
+import dayjs from "dayjs";
+
 export const promisifyCallback = (thisArg: any, func: any, ...args: any) =>
 	new Promise((resolve, reject) =>
 		func.call(thisArg, ...args, (err: any, data: any) =>
@@ -28,3 +31,41 @@ export const isObjectId = (possibleObjectId: any): boolean =>
 			? possibleObjectId
 			: possibleObjectId.toString()
 	);
+
+export const isPrimitive = (val: unknown) =>
+	!val || (typeof val !== "object" && !(val instanceof Date));
+
+
+export const applyTimezone = (date: Date, timezone: string) =>
+	timezone === "local"
+		? dayjs.utc(date).tz(dayjs.tz.guess()).format()
+		: date.toISOString();
+
+export const formatBsonDocument = (elem: Ark.BSONTypes, timezone: string) => {
+	if (isPrimitive(elem)) {
+		return elem;
+	} else if (elem instanceof Date) {
+		return `ISODate('` + applyTimezone(elem, timezone) + `')`;
+	} else if (Array.isArray(elem)) {
+		return elem.map((elem) => formatBsonDocument(elem, timezone));
+	} else if (
+		ObjectId.isValid(elem as Extract<Ark.BSONTypes, string | ObjectId>) &&
+		elem !== null
+	) {
+		return `ObjectId('` + elem.toString() + `')`;
+	} else if (typeof elem === "object" && elem !== null) {
+		return Object.keys(elem).reduce(
+			(acc, key) => ((acc[key] = formatBsonDocument(elem[key], timezone)), acc),
+			{}
+		);
+	} else if (elem === null) {
+		return "null";
+	}
+};
+
+export const formatBSONToText = (doc: Ark.BSONArray, timezone = "local") =>
+	Array.isArray(doc)
+		? doc.map((elem) => formatBsonDocument(elem, timezone))
+		: typeof doc === "object"
+			? formatBsonDocument(doc, timezone)
+			: doc;
