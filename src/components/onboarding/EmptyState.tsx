c7e8e809621b41d944b11f6725d72420f0bef756
Shashank Kaul <kaulshashank96@gmail.com>
2022-08-14 00:24:18 +0530

    feat: Logo and browser empty state changes

--- src/components/onboarding/EmptyState.tsx
+++ src/components/onboarding/EmptyState.tsx
@@ -6,10 +6,18 @@ import { ReactComponent as Logo } from "../../assets/logo_fill.svg";
 export const EmptyState: FC = () => {
 	return (
 		<div className="empty-state">
-			<Logo height={"35rem"} width={"35rem"} opacity={"50%"} />
+			<Logo height={"25rem"} width={"25rem"} opacity={"40%"} />
 			<div className="text-row">
 				<div className="text-row-key">See connections</div>
 				<code className="text-row-value">Ctrl/Cmd + E</code>
+			</div>
+			<div className="text-row">
+				<div className="text-row-key">New connection</div>
+				<code className="text-row-value">Ctrl/Cmd + N</code>
+			</div>
+			<div className="text-row">
+				<div className="text-row-key">Open a script</div>
+				<code className="text-row-value">Ctrl/Cmd + O</code>
 			</div>
 		</div>
 	);
